b9c1fd766511321f9fc9f7428d84467196184fb9
Rithul Kamesh <hi@rithul.dev>
2022-06-01 20:25:11 +0530

    Made task script work everywhere

--- scripts/task.py
+++ scripts/task.py
@@ -1,10 +1,14 @@
 import argparse
+import os
 
-path = "/Users/rithulkamesh/Library/Application Support/Übersicht/widgets/currentTask/data.json"
+path = os.path.expanduser('~') + "/Library/Application Support/Übersicht/widgets/currentTask/data.json"
 # get args from command line and set the task in path
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--task", help="set the task")
 args = parser.parse_args()
+
+if not args.task:
+    exit("\n\033[91mERROR:\033[0m No task specified\n\033[94mUsage:\033[0m \033[1m task.py -t <task>")
 
 # Open the file and set the object's task as the task from args
 with open(path, "w") as f:
